ac8f655da8214d4cf300df9b5ce6e2ac4a749f15
givagusliana <givagusliana21@gmail.com>
2026-06-20 20:22:01 +0700

    Fix: Enable CORS for frontend connection

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Sequelize, DataTypes } = require('sequelize');
 const cors = require('cors');
 require('dotenv').config();
-
+app.use(cors({ origin: '*' }));
 const app = express();
 const PORT = process.env.PORT || 5000; // Menggunakan port 5000 agar tidak bentrok dengan frontend
 
